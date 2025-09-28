49fc5377773e2ee074f69efa28b91bdf8af4dd80
Komron-Nazarov <komron7nazarov@gmail.com>
2025-09-28 17:25:22 +0500

    Заменил ненужные div на Link на стр. page.jsx

--- app/quizChoice/page.jsx
+++ app/quizChoice/page.jsx
@@ -1,26 +1,33 @@
-import Image from "next/image"
-import quizBg from "@/app/assets/quizBg.png"
-import einstein from "@/app/assets/einstein.png"
-import alien from "@/app/assets/alien.png"
-import eilien from "@/app/assets/eilien.png"
+import Image from "next/image";
+import Link from "next/link";
 
-
+import quizBg from "@/app/assets/quizBg.png";
+import einstein from "@/app/assets/einstein.png";
+import alien from "@/app/assets/alien.png";
+import eilien from "@/app/assets/eilien.png";
 
 export default function Tests() {
-    return (
-      <div id="quiz" className="container mx-auto w-screen h-screen">
-        <Image src={quizBg} alt="starry-background" className="w-screen relative" />
-        <div className="flex absolute top-48 left-36 gap-8">
-          <div>
-            <Image src={einstein} alt="einsteinCard "  />
-          </div>
-          <div>
-            <Image src={alien} alt="alienCard" className="mt-56" />
-          </div>
-          <div>
-            <Image src={eilien} alt="einlienCard" />
-          </div>
-        </div>
+  return (
+    <div id="quiz" className="container mx-auto w-screen h-screen relative">
+      <Image
+        src={quizBg}
+        alt="starry-background"
+        className="w-screen object-cover"
+      />
+
+      <div className="flex absolute top-48 left-36 gap-8">
+        <Link href="/quiz">
+          <Image src={einstein} alt="einsteinCard" className="cursor-pointer" />
+        </Link>
+
+        <Link href="/quiz">
+          <Image src={alien} alt="alienCard" className="mt-56 cursor-pointer" />
+        </Link>
+
+        <Link href="/quiz">
+          <Image src={eilien} alt="eilienCard" className="cursor-pointer" />
+        </Link>
       </div>
-    );
+    </div>
+  );
 }
